5335137180c5b3f2ea9e068ecd0e2aed2e0d1580
roman <romaxid@yandex.by>
2024-09-26 21:34:20 +0300

    feat: init

---
+++ src/apps/users/api/v1/controllers/user.py
@@ -0,0 +1,26 @@
+from http.client import HTTPException
+
+from modules.auth import get_password_hash
+from modules.responses.user import responses as user_responses
+from fastapi import APIRouter, status
+
+from core.dao import UserDAO
+from fastapi.responses import JSONResponse
+
+from schemas.user import CreateUserSchema
+
+user_router = APIRouter(prefix="/api/v1/profile", tags=["Profile"])
+
+
+@user_router.post(path="/register", summary="Регистрация пользователя")
+async def create_user(registration_user_data: CreateUserSchema) -> JSONResponse:
+    user = await UserDAO.get_one_or_none(registration_user_data.email)
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=user_responses.USER_ALREADY_EXISTS,
+        )
+    user_dict = registration_user_data.model_dump()
+    user_dict["password_1"] = get_password_hash(registration_user_data.password_1)
+    new_user = await UserDAO.add(**user_dict)
+    return JSONResponse(status_code=201, content=f"Успешно. Вот юзер: {new_user}")
